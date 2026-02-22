9377a354b59cde407afe86329429773b7c612271
Nico <pmontalvo@cardinalspellman.edu.ec>
2026-02-22 17:02:03 -0500

    Configure CORS for production

--- servidor/src/app.js
+++ servidor/src/app.js
@@ -6,21 +6,27 @@ const userRoutes = require("./routes/users.routes");
 const quizRoutes = require("./routes/quiz.routes");
 const dashboardRoutes = require("./routes/dashboard.routes");
 
-const app = express(); // 👈 PRIMERO se crea la app
+const app = express();
 const alumnosRoutes = require("./routes/alumnos.routes");
-// Middlewares
+
 app.use(express.json());
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: [
+    "http://localhost:3000",
+    "https://ecumentis.org",
+    "https://www.ecumentis.org"
+  ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
-app.options("*", cors()); 
-// Rutas
+
+app.options("*", cors());
+
 app.use(authRoutes);
 app.use(userRoutes);
 app.use(quizRoutes);
 app.use(dashboardRoutes);
 app.use(alumnosRoutes);
+
 module.exports = app;
