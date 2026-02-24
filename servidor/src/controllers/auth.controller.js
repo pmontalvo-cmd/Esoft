9bf65f097de9a5d2dc392aa848b9f442b31ffef4
Nicolas Schreckinger <138933961+NicolasSch09@users.noreply.github.com>
2026-02-23 20:35:29 -0500

    Update auth.controller.js

--- servidor/src/controllers/auth.controller.js
+++ servidor/src/controllers/auth.controller.js
@@ -2,10 +2,9 @@ const db = require("../config/database")
 function login(req, res) {
     const { username, password } = req.body;
 
-    db.query(
-        "SELECT id, username, grade FROM datos_usuario WHERE username=? AND password=?",
-        [username, password],
-        (err, rows) => {
+    console.log("LOGIN HIT");
+    db.query("SELECT id, username, grade FROM datos_usuario WHERE username=? AND password=?",[username, password], (err, rows) => {
+            console.log("LOGIN CALLBACK", { err, rowsLen: rows?.length });
             if (err) return res.status(500).send("DB error", err);
             if (rows.length === 0) return res.status(401).send("Invalid credentials");
 
