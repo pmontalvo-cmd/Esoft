c936869060744be666770da1d3b0f05533d3777d
Nicolas Schreckinger <138933961+NicolasSch09@users.noreply.github.com>
2026-02-23 19:59:27 -0500

    Update app.js

--- servidor/src/app.js
+++ servidor/src/app.js
@@ -16,7 +16,6 @@ app.use(cors({
     "http://localhost:3000",
     "https://ecumentis.org",
     "https://www.ecumentis.org"
-    "https://api.ecumentis.org"
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
