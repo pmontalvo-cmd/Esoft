ef54231083a71893f27c18dff7cd8feebae5276b
pmontalvo-cmd <pmontalvo@cardinalspellman.edu.ec>
2026-02-23 22:46:13 -0500

    Update app.js

--- servidor/src/app.js
+++ servidor/src/app.js
@@ -1,56 +1,35 @@
 const express = require("express");
 const cors = require("cors");
-const db = require("./config/database")
+const db = require("./config/database");
 
 const authRoutes = require("./routes/auth.routes");
 const userRoutes = require("./routes/users.routes");
 const quizRoutes = require("./routes/quiz.routes");
 const dashboardRoutes = require("./routes/dashboard.routes");
-
-const app = express();
 const alumnosRoutes = require("./routes/alumnos.routes");
 
-app.use(express.json());
+const app = express();
+
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://ecumentis.org",
+  "https://www.ecumentis.org"
+];
 
 app.use(cors({
-  origin: [
-    "http://localhost:3000",
-    "https://ecumentis.org",
-    "https://www.ecumentis.org"
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  origin: allowedOrigins,
   credentials: true
 }));
 
+app.options("*", cors({
+  origin: allowedOrigins,
+  credentials: true
+}));
 
-app.options("*", cors());
+app.use(express.json());
 
-app.get("/health", (req, res) => res.status(200).json({ ok: true })); // Health Tets
+app.get("/health", (req, res) => res.status(200).json({ ok: true }));
 
-// DB Test
-app.get("/db-test", async (req, res) => {
-  try {
-    const [rows] = await db.query("SELECT 1 AS ok");
-    return res.json({ ok: true, rows });
-  } catch (err) {
-    console.error("DB TEST ERROR:", err);
-    return res.status(500).json({
-      ok: false,
-      name: err.name,
-      code: err.code,
-      errno: err.errno,
-      message: err.message,
-    });
-  }
-});
-// Forces Timeout
-app.use((req, res, next) => {
-  res.setTimeout(15000, () => {   // 15s
-    return res.status(504).json({ ok: false, message: "Request timeout" });
-  });
-  next();
-});
 app.use(authRoutes);
 app.use(userRoutes);
 app.use(quizRoutes);
