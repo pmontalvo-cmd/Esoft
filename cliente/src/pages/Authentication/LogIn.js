e883eaa6ab402f74f410a13bb7e9c291579c2c4e
pmontalvo-cmd <pmontalvo@cardinalspellman.edu.ec>
2026-02-23 20:28:36 -0500

    Update LogIn.js

--- cliente/src/pages/Authentication/LogIn.js
+++ cliente/src/pages/Authentication/LogIn.js
@@ -17,7 +17,7 @@ function Login({ onLoginSuccess }) {
     try {
       console.log("Attempting login with:", username, password);
 
-      const response = await fetch("https://api.ecumentis.org/api/login", {
+      const response = await fetch("https://api.ecumentis.org/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
