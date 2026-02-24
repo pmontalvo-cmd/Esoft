fb7a14ccace6af47cad637cfdff13f8d19341153
Nicolas Schreckinger <138933961+NicolasSch09@users.noreply.github.com>
2026-02-23 20:28:48 -0500

    Update auth.routes.js

--- servidor/src/routes/auth.routes.js
+++ servidor/src/routes/auth.routes.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 const authController = require("../controllers/auth.controller");
 
-router.post("/login", authController.login);
-
+// router.post("/login", authController.login);
+router.post("/login", (req, res) => {
+  console.log("LOGIN BODY:", req.body);
+  return res.json({ ok: true });
+});
 module.exports = router;
