2017cb962707c54adffd236caf24a6c9ed0dfb77
Nicolas Schreckinger <138933961+NicolasSch09@users.noreply.github.com>
2026-02-23 20:20:09 -0500

    Update auth.controller.js
    Included error in erro message for /login

--- servidor/src/controllers/auth.controller.js
+++ servidor/src/controllers/auth.controller.js
@@ -6,7 +6,7 @@ function login(req, res) {
         "SELECT id, username, grade FROM datos_usuario WHERE username=? AND password=?",
         [username, password],
         (err, rows) => {
-            if (err) return res.status(500).send("DB error");
+            if (err) return res.status(500).send("DB error", err);
             if (rows.length === 0) return res.status(401).send("Invalid credentials");
 
             res.json(rows[0]); // Send user data without password
