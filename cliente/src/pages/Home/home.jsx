720459c2e1bfde2152193005068872c7e229164d
Nicolas schreckinger <nicolas.schreckinger@fcaq.k12.ec>
2026-02-27 22:00:34 -0500

    Fixed: missing use navigate import on home

--- cliente/src/pages/Home/home.jsx
+++ cliente/src/pages/Home/home.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./home.css";
 
 const quickRoutes = [
@@ -20,6 +20,7 @@ const subjects = [
 
 
 export default function Home() {
+const navigate = useNavigate();
 
 let [query, setQuery] = useState("");
 return (
