a6b37b5c3f34d20a6ae4d173cf6ca86566ded7e4
Nicolas schreckinger <nicolas.schreckinger@fcaq.k12.ec>
2026-03-02 15:29:07 -0500

    IMPORTANT !(DO NOT TOCH) Added i18n to api service in client. Fully implemented i18n expected

--- cliente/src/services/api.js
+++ cliente/src/services/api.js
@@ -1,8 +1,15 @@
 import axios from "axios";
+import { getLang } from "../i18n";
 
 const API = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   withCredentials: true,
+});
+
+API.interceptors.request.use((config) => {
+  const lang = getLang(); // "es" o "en"
+  config.params = { ...(config.params || {}), lang };
+  return config;
 });
 
 export default API;
