77cde0fa238426de9c2f93ec64f6a8aff471d794
Nicolas schreckinger <nicolas.schreckinger@fcaq.k12.ec>
2026-03-03 01:50:23 -0500

    FIXED top bar in dashboard

--- cliente/src/pages/Dashboard/Dashboard.jsx
+++ cliente/src/pages/Dashboard/Dashboard.jsx
@@ -166,9 +166,8 @@ useEffect(() => {
 
   // Return
   return (
-  <div className="dashboard-shell">
+  <div className="dashboard-shell section section--topband">
     <div className="hero-band">
-  <section className="section section--topband">
       <Container className="page dashboard-page">
         {/* Header */}
         <Row className="mb-3">
@@ -250,7 +249,6 @@ useEffect(() => {
           </Col>
         </Row>
       </Container>
-  </section>
 
         {/* Recommended */}
         <Container className="py-4">
